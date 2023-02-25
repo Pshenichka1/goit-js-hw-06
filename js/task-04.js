8d545e06f092d02105806c985ba98722dc54419d
Pshenichka1 <pshenichka.olga@gmail.com>
2023-02-25 02:19:07 +0200

    task 04

---
+++ js/task-04.js
@@ -0,0 +1,16 @@
+const refs = {
+    decrement: document.querySelector('button[data-action="decrement"]'),
+    increment: document.querySelector('button[data-action="increment"]'),
+    span: document.querySelector('#value'),
+};
+const handleBtnClick = () => {
+
+}
+let counterValue = 0;
+refs.decrement.addEventListener('click',  () => {
+    refs.span.textContent = counterValue -= 1;
+});
+
+refs.increment.addEventListener('click', () => {
+    refs.span.textContent = counterValue += 1;
+});
