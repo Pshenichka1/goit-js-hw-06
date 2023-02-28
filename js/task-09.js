d7c82529658756a1d8a57d0a41dd9939ff98a4c9
Pshenichka1 <pshenichka.olga@gmail.com>
2023-02-28 20:10:03 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeColorBtn = document.querySelector('.change-color');
+const nameColorSpan = document.querySelector('.color');
+const colorBody = document.body;
+changeColorBtn.addEventListener('click', (event) => {
+  colorBody.style.backgroundColor = getRandomHexColor();
+  nameColorSpan.textContent = getRandomHexColor();
+})
